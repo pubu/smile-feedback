0699bf535038ba20bf22f3a43bce454752547ad6
paul.dircksen <paul.dircksen>
2018-05-29 21:33:40 +0200

    update frontend

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -1,7 +1,9 @@
 // JS Goes here - ES6 supported
 $('input#submitButton').click( function() {
     $.post( 'https://www.smile-feedback.de/.netlify/functions/service', $('form#emailForm').serialize(), function(data) {
-         console.log(data)
+         var img = new Image();
+         img.src = "data:image/png;base64," + data.qr;
+         $('form#emailForm').append(img);
        },
        'json' // I expect a JSON response
     );
